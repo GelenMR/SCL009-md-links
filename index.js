66ac5bd45d5fe1ab7352b91aba0af539f7e1f6be
GelenMR <gelen.montilla.r@gmail.com>
2019-07-07 21:53:45 -0400

    HU5 en proceso: Opcion Stats Lista

--- index.js
+++ index.js
@@ -2,25 +2,30 @@
 
 const mdLinks = require('./md-links.js')
 const path = require('path');
+const chalk = require('chalk');
 /* GUARDA EN UNA VARIABLE EL ARGUMENTO QUE ESCRIBE EL USUARIO 
 EN LA TERMINAL(PATH) */
 let pathToFile = process.argv[2];
-console.log('path to file:', pathToFile);
+//console.log('path to file:', pathToFile);
 // Path Absoluto
 pathToFile = path.resolve(pathToFile);
 pathToFile = path.normalize(pathToFile);
-console.log('path absolute:', pathToFile);
+console.log(chalk.green('Path Absolute:', pathToFile));
 // Argumeto en index 3
 let option1 = process.argv[3];
-console.log('Option 1 to file:', option1);
+//console.log('Option 1 to file:', option1);
 // Argumeto en index 4
 let option2 = process.argv[4];
-console.log('Option 2 to file:', option2);
+//console.log('Option 2 to file:', option2);
 
 mdLinks(pathToFile, option1, option2)
     .then(res=>{
-        console.log('Soy Index:',res);
+        //console.log(chalk.yellow('Links Encontrados - Index:',res));
+        console.log(chalk.yellow('Links Encontrados - Index:'),res);
+        if(res.length == 0){
+          console.log(chalk.red('No se encontraron Links dentro del Archivo.'));
+        }
       })
       .catch(err=>{
-        console.log('Soy Error Index:',err);
+        //console.log(chalk.red('Error - Index:',err));
       })
--- md-links.js
+++ md-links.js
@@ -18,15 +18,24 @@ const isDirOrFile = (path) =>{
           console.log(chalk.red('Ruta No Valida. Por Favor, Ingresar Ruta Valida.'));
         }  
       }else if(stats.isDirectory()){
-      console.log('Is Directory?', stats.isDirectory());
+      console.log(chalk.blue('La Ruta Ingresda es un Directorio'));
         readDir(path)
         .then(res =>{
-          console.log(chalk.blue('La Ruta Ingresda es un Directorio'));
-          resolve(res);
-        })
+          Promise.all(res.map(files =>{
+            console.log(chalk.blue(files));
+            return readFile(files);
+          }))
+          .then(res=>{
+            let arrLinks = [];
+            res.forEach(links=>{links.map(link=>arrLinks.push(link))
+              //console.log(arrLinks);
+              resolve(arrLinks);
+            })
+            })
+          })
         .catch(err =>{
           reject(err);
-        })
+        }) 
       }else{ 
         fileMD(path)
         .then(res =>{
@@ -47,10 +56,8 @@ const fileMD = (file) =>{
     // Extención de archivo
     let ext = path.extname(file);
     console.log(chalk.green('Este Archivo es:'), chalk.yellow(ext));
-    let arrayFile =[];
     if(ext === '.md'){
-      arrayFile.push(file);
-      readFile(arrayFile)
+      readFile(file)
       .then(res=>{
         resolve(res);
       })
@@ -64,29 +71,27 @@ const fileMD = (file) =>{
 }
 
 //FUNCION PARA LEER ARCHIVO/LINKS Y CREAR ARRAY 
-const readFile = (files) =>{
-  return Promise.all(files.map(file =>{
-    return new Promise ((resolve,reject) =>{
-      fs.readFile(file,'utf8', (err, data) => {
-        if (err){
-          reject(err);
-        }else{
-        let links = [];
-        // metodo para que cree una nueva instancia en el array
-        const renderer = new marked.Renderer();
-        renderer.link = function (herf, title, text){
-        links.push({
-          file:file,
-          herf:herf,
-          text:text
-        })
-        }
-        marked(data,{renderer:renderer});
-        resolve(links);
-        }
+const readFile = (file) =>{
+  return new Promise ((resolve,reject) =>{
+    fs.readFile(file,'utf8', (err, data) => {
+      if (err){
+        reject(err);
+      }else{
+      let links = [];
+      // metodo para que cree una nueva instancia en el array
+      const renderer = new marked.Renderer();
+      renderer.link = function (herf, title, text){
+      links.push({
+        file:file,
+        herf:herf,
+        text:text
       })
+      }
+      marked(data,{renderer:renderer});
+      resolve(links);
+      }
     })
-  }))
+  })
 }
 
 // FUNCION LEER/RECORRER UN DIRECTORIO EN BUSCA DE ARCHIVOS .MD CON FILEHOUND
@@ -102,16 +107,10 @@ const readDir = (path) =>{
       }
     })
    .then(files =>{
-      files.forEach(file =>{
-        readFile(files)
-        .then(res =>{
-          resolve(res);
-        })
-        .catch(err =>{
-            reject(err);
-        })
-      })
-      console.log('Archivos:', files);
+      resolve(files);
+    })
+    .catch(err =>{
+        reject(err);
     })
   })
 }
@@ -121,10 +120,10 @@ const linksToValidate = (path) =>{
   return new Promise((resolve, reject) =>{
     isDirOrFile(path)
     .then(res=>{
+      console.log(chalk.yellow('Ingreso Opción Validate.'));
       validateOption(res)
       .then(res=>{
         resolve(res);
-        //console.log(chalk.yellow('Ingreso Opción Validate.'), 'Links To Validate:', res);
       })
       .catch(err=>{
         reject(err);
@@ -138,21 +137,51 @@ const linksToValidate = (path) =>{
 
 // FUNCIÓN PARA EXTRAER INFO DEL LINK (PETICIÓN HTTP CON FETCH)
 const validateOption = (files) =>{
-  return Promise.all(files.map(links =>{
-    return Promise.all(links.map(link=>{
-      return new Promise((resolve,reject)=>{
-        fetch(link.herf)
-        .then(res=>{
-          link.status = res.statusText;
-          link.code = res.status;
-          resolve(link);
-        })
-        .catch(err=>{
-          reject(err);
-        })
+  return Promise.all(files.map(link =>{
+    return new Promise((resolve,reject)=>{
+      fetch(link.herf)
+      .then(res=>{
+        link.status = res.statusText;
+        link.code = res.status;
+        resolve(link);
       })
-    }))
+      .catch(err=>{
+        //reject(err);
+        if(err){
+          link.status = "FAIL";
+          link.response = null;
+          resolve(link);
+        }
+      })
+    })
   }))
+}
+
+// FUNCIÓN PARA EXTRAER INFO DEL LINK (STAST)
+const statsOption = (path) =>{
+  let totalLinks = 0;
+  let arrLinks = [];
+  let uniqueLinks = 0;
+  let brokenLinks = [];
+  return new Promise((resolve, reject) =>{
+    isDirOrFile(path)
+    .then(links=>{
+      arrLinks = links.map(link =>link.herf)
+      totalLinks = links.length;
+      uniqueLinks = [...new Set(arrLinks)].length;
+      console.log(chalk.green('Links Totales:'), totalLinks, arrLinks)
+      objectTotalLinks = {
+        Total: totalLinks,
+        Unique: uniqueLinks,
+        //Broken: brokenLinks
+      };
+      resolve(objectTotalLinks);      
+      })
+    })
+    .catch(err=>{
+      reject(err);
+    })
+  
 }
 
 // VERIFICAR SI EL USUARIO INTRODUJO OPTIONS
@@ -166,20 +195,20 @@ const mdLinks = (path,option1,option2) =>{
     }else if(option1 === '--validate' && option2 === '--stats' || option1 === '--stats' && option2 === '--validate'){
       validate = true;
       stats = true;
-      console.log(chalk.yellow('Ingreso Opciones Validate y Stats'));
-      resolve();
+      console.log(chalk.yellow('Ingreso Opciones: Validate y Stats. Por Favor Ingresa solo una Opción.'));
+      //resolve();
     }else if(option1 === '--validate' && option2 === undefined){
       validate = true;
       resolve(linksToValidate(path))
     }else if(option1 === '--stats' && option2 === undefined){
       stats = true;
       console.log(chalk.yellow('Ingreso Opcion Stats'));
-      //resolve(statsOption(path));
+      resolve(statsOption(path));
     }else{
       console.log(chalk.red('Opcion No Valida.'),'Las Opciones validas son:\n --validate\n --stats\n --validate --stats');
     }
-    console.log('Validate:', validate);
-    console.log('Stats:', stats);
+    // console.log('Validate:', validate);
+    // console.log('Stats:', stats);
   })
 }
 module.exports = mdLinks;
